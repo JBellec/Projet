8636cf2d4408ff6c2f8c32cdc480ceee4640b3f1
zoski <zoski@free.fr>
2014-05-29 11:53:44 +0200

    Nabar modifié

--- includes/navbar.php
+++ includes/navbar.php
@@ -13,26 +13,14 @@
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link</a></li>
-                    <li><a href="profile.pgp">Mon profile</a></li>
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                    </li>
+                    <!--<li class="active"><a href="#">Link</a></li>-->
+                    
+                    <li><a href="qrxid.php">QR Xid</a></li>
+                    <!-- Menu déroulant pour les admin à faire includes/navbar_admin.php-->
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="./">Default</a></li>
-                    <li><a href="../navbar-static-top/">Static top</a></li>
-                    <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+                    <li><a href="profile.php">Me</a></li>
+                    <li><a href="logout.php">Logout</a></li><!-- ICI JULIEN -->
                 </ul>
             </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
