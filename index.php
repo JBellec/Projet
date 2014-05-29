213b24ea846b4b0a082bb7561d8450a3a59ac232
daliadadie <dalia33@hotmail.fr>
2014-05-29 11:21:13 +0200

    index page

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>Justified Nav Template for Bootstrap</title>
+    <?php include('logout.php'); ?>
+   <?php include('login.php'); ?>
+   <!-- Custom styles for this template -->
+    <link href="css/projet.css" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="justified-nav.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    
+
+    
+
+      
+
+    
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Our fun project!</h1>
+        <p class="lead">Hi everybody and welcome on our wesite :) . Yes your in the right one ! Here you can scan a QR Code in order to find  your classmates, being his friend, and else. You can also borrow stuff from XID so easily !</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button"> Don't hesitate and get started today</a></p>
+      </div>
+
+     
+
+      <!-- Example row of columns -->
+      <div class="row">
+        
+        <div class="col-lg-3">
+          <h2>XID QRcode</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-lg-3">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-3">
+
+
+                   
+                        <form class="form-signin" role="form">
+                          <h2 class="form-signin-heading">Please sign in</h2>
+                          <input type="email" class="form-control" placeholder="Email address" required autofocus>
+                          <input type="password" class="form-control" placeholder="Password" required>
+                          <label class="checkbox">
+                            <input type="checkbox" value="remember-me"> Remember me
+                          </label>
+                          <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                          <button type="button" class="btn btn-primary btn-lg btn-block" onclick="self.location.href='Inscription2.html'">Inscription</button>
+                        </form>
+
+                   s
+
+                </div>
+
+      <!-- Site footer -->
+      <div class="footer">
+        <p>&copy; ENSISA 2014</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
