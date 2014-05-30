ec0a7ef633c460d544d7e1efa0c28073e3cd66f3
Tumai <julien.bellec@hotmail.fr>
2014-05-30 14:27:32 +0200

    Ajout de closeCursor() dans le fichier navbar_test.php

--- includes/navbar_test.php
+++ includes/navbar_test.php
@@ -6,7 +6,7 @@ if(isset($_SESSION['login'])) { //$_SESSION['login'] = pseudo
 		$req->execute(array($_SESSION['pseudo']));
 		$resultat=$req->fetch();
 		$isadmin = $resultat[0];
-		
+		$req->closeCursor();
 		if($isadmin==1) {
         include 'navbar_isadmin.php';
 		} 
