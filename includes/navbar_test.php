e4090db8f0d1d717da46559d1a38c0e06a3bf3fe
Tumai <julien.bellec@hotmail.fr>
2014-05-30 11:05:09 +0200

    navbar_test.php commentée

--- includes/navbar_test.php
+++ includes/navbar_test.php
@@ -1,12 +1,12 @@
 <?php 
 if(isset($_SESSION['login'])) { //$_SESSION['login'] = pseudo
         include 'mysql.php';
-        $req='SELECT admin FROM user WHERE pseudo='.$_SESSION['login'];
-        $isadmin=$connect->query($req);
-        $fetched=$isadmin->fetch();
+       // $req='SELECT admin FROM user WHERE pseudo='.$_SESSION['login'];
+       // $isadmin=$connect->query($req);
+       // $fetched=$isadmin->fetch();
 		
-		if($fetched==1) {
-        include 'navbar_isadmin.php';
-		} 
+		//if($fetched==1) {
+        //include 'navbar_isadmin.php';
+		//} 
 }               
 ?> 
