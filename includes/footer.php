4664691cfdc33d75a16db93b3137a9a77021a41b
zoski <zoski@free.fr>
2014-05-30 10:44:24 +0200

    renommage & admin panel

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    
+    </body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-2.1.0.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    
 </html>
--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="lib/font-awesome-4.1.0/css/font-awesome.min.css">
 
-
+    
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
