d1a575fe6b0fb4eb2d862db813337e96f480843f
Tumai <julien.bellec@hotmail.fr>
2014-05-30 14:30:13 +0200

    navbar_isadmin.php  apparait et disparait bien lors de la connexion et deconnexion

--- includes/navbar_test.php
+++ includes/navbar_test.php
@@ -1,5 +1,5 @@
 <?php 
-if(isset($_SESSION['login'])) { //$_SESSION['login'] = pseudo
+if(isset($_SESSION['pseudo'])) { //$_SESSION['login'] = pseudo
         include 'mysql.php';
 
 		$req = $connect->prepare('SELECT admin FROM user WHERE pseudo = ?');
@@ -7,6 +7,7 @@ if(isset($_SESSION['login'])) { //$_SESSION['login'] = pseudo
 		$resultat=$req->fetch();
 		$isadmin = $resultat[0];
 		$req->closeCursor();
+		
 		if($isadmin==1) {
         include 'navbar_isadmin.php';
 		} 
