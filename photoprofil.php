60f73bd5b5ca2a4008c6abed9b45e9b416ce7485
Riimaa <elamri.rima@hotmai.fr>
2014-05-30 11:33:06 +0200

    Pour modifier les photos de profil

---
+++ photoprofil.php
@@ -0,0 +1,31 @@
+<?php include 'includes/header.php'; 
+ include 'includes/navbar.php'; ?>
+
+	<title>Profil</title>
+ </head> 	
+    <body>
+          <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4"> <!--debut colonne 1 -->
+          <img class="img-circle" src="image/img.png" alt="Generic placeholder image">
+          <h2>Photo 1</h2>
+         
+          <p><a class="btn btn-default" href="#" role="button">Modifier la photo ;) &raquo;</a></p>
+        </div><!-- fin colonne 1 -->
+        <div class="col-lg-4"> <!-- debut colonne 2 -->
+          <img class="img-circle" src="image/img.png" alt="Generic placeholder image">
+          <h2>Photo 2</h2>
+          
+          <p><a class="btn btn-default" href="#" role="button">Modifier la photo ;) &raquo;</a></p>
+        </div><!-- fin colonne 2 -->
+        <div class="col-lg-4"> <!-- debut colonne 3 -->
+          <img class="img-circle" src="image/img.png" alt="Generic placeholder image">
+          <h2>Photo 3</h2>
+          
+          <p><a class="btn btn-default" href="#" role="button">Modifier la photo ;) &raquo;</a></p>
+        </div><!-- fin colonne -->
+      </div><!-- /.row -->
+	 
+	</body>
+
+<?php include 'includes/footer.php' ?>
