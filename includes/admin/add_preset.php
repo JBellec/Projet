566432299e97f2db10ecf8be22d77ad2029a3c42
zoski <zoski@free.fr>
2014-05-31 10:38:01 +0200

    début formulaire ajout preset

--- includes/admin/add_preset.php
+++ includes/admin/add_preset.php
@@ -35,7 +35,57 @@
     </table>
 </div> <!-- /table-responsive-->
 
-<h2 class="sub-header">Add preset</h2>
+<h2 class="sub-header">Add a preset</h2>
+<form role="form">
+    <div class="form-group">
+        <label for="presetName">Preset Name</label>
+        <input type="text" class="form-control" id="presetName" placeholder="Enter the preset name">
+    </div>
+    <div class="form-group">
+        <label for="description">Descrition</label>
+        <input type="text" class="form-control" id="description" placeholder="Description, Keep it short ;-)">
+    </div>
+    <!-- -->
+     <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Lenght</th>
+                <th>Port numb</th>
+                <th>CPU</th>
+                <th>Freq</th>
+                <th>RAM</th>
+                <th>HDD</th>
+                <th>GPU</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox"> Check me out
+                        </label>
+                    </div>
+                </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="checkbox">
+        <label>
+        <input type="checkbox"> Check me out
+        </label>
+    </div>
+<button type="submit" class="btn btn-default">Submit</button>
+</form>
+
+
 
 
 
