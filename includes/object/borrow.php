4960a298f715e7749166e1528f34ab709edf7ab5
Tumai <julien.bellec@hotmail.fr>
2014-05-31 13:02:03 +0200

    boutton borrow

--- includes/object/borrow.php
+++ includes/object/borrow.php
@@ -1,4 +1,2 @@
  
-<form method="post" action="includes/admin/act_borrower.php">
-	<button type="submit" class="btn btn-primary btn-medium"><i class="fa fa-pencil-square-o"></i> Borrow</button>
-</form>
+
