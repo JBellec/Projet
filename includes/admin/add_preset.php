b8474ef74e498c2433384914fc6d17e333bb0d75
zoski <zoski@free.fr>
2014-05-31 10:00:38 +0200

    tjrs l'administration des preset

---
+++ includes/admin/add_preset.php
@@ -0,0 +1,38 @@
+<h1 class="page-header">Preset administration</h1>
+<?php include 'includes/mysql.php';
+    /* On se connecte est on récupérer tous les preset deja existant que l'on affiche dans un tableau */
+    
+
+
+?>
+<h2 class="sub-header">Available Presets</h2>
+<div class="table-responsive">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Preset Name</th>
+                <th>Lenght</th>
+                <th>Port numb</th>
+                <th>CPU</th>
+                <th>Freq</th>
+                <th>RAM</th>
+                <th>HDD</th>
+                <th>GPU</th>
+            </tr>
+        </thead>
+    <tbody>
+        <tr>
+            <td>1,001</td>
+            <td>1,001</td>
+            <td>Lorem</td>
+            <td>ipsum</td>
+            <td>dolor</td>
+            <td>sit</td>
+            <td>dolor</td>
+            <td>dolor</td>
+        </tr>
+        </tbody>
+    </table>
+</div> <!-- /table-responsive-->
+
+<h2 class="sub-header">Add preset</h2>
