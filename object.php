7933b0d4ccd841639740ade9824d11b24ade07f0
Tumai <julien.bellec@hotmail.fr>
2014-06-04 21:24:18 +0200

    modif description objet

--- object.php
+++ object.php
@@ -10,39 +10,41 @@
 <title>Object - <?php echo $object_name?></title>
 <?php include 'includes/navbar.php'; ?>
 <div class="container">
-    <?php 
-        if(isset($_GET['qr'])) { /* Pour un accès par qr Code */
-            if(isset($_SESSION['pseudo'])){
-				include 'includes/object/description.php';
-                
-				if($result['borrowed']==0) { // si l'objet n'est pas emprunté le bouton s'affiche ?>
-					<form method="post" action="includes/admin/act_borrower.php?id=<?php echo $_GET['id'] ?>">
-					    <?php echo '<input type="submit" value="Borrow" class="btn btn-primary btn-medium" />'; 
-					echo '</form>';
-				//si on est connecté on peu emprunter
-				} else {
-					echo "Objet déjà emprunté";
-				}
-			} else {  //sinon il est nécessaire de se connecter
-                include "include/object/login.php";
-            }
-        } else {/* On fait un accès direct depuis l'administration par exe */
-            if(isset($_SESSION['pseudo'])){
+	<div class="jumbotron">
+		<?php 
+			if(isset($_GET['qr'])) { /* Pour un accès par qr Code */
+				if(isset($_SESSION['pseudo'])){
 					include 'includes/object/description.php';
-                   
-				    if($result['borrowed']==0){ // si l'objet n'est pas emprunté le bouton s'affiche?>
+					
+					if($result['borrowed']==0) { // si l'objet n'est pas emprunté le bouton s'affiche ?>
 						<form method="post" action="includes/admin/act_borrower.php?id=<?php echo $_GET['id'] ?>">
-							<input type="submit" value="Borrow" class="btn btn-primary btn-medium" />
-						</form><?php //si on est connecté on peu emprunter
-					} else{
+							<?php echo '<input type="submit" value="Borrow" class="btn btn-primary btn-medium" />'; 
+						echo '</form>';
+					//si on est connecté on peu emprunter
+					} else {
 						echo "Objet déjà emprunté";
 					}
-					
-					//include 'includes/object/borrow.php';//on affiche de quoi emprunter l'objet
-             } else {
-                    echo 'You should login to borrow this item'; // on doit se connecter
-                }
-            }    
-    ?>  
+				} else {  //sinon il est nécessaire de se connecter
+					include "include/object/login.php";
+				}
+			} else {/* On fait un accès direct depuis l'administration par exe */
+				if(isset($_SESSION['pseudo'])){
+						include 'includes/object/description.php';
+					   
+						if($result['borrowed']==0){ // si l'objet n'est pas emprunté le bouton s'affiche?>
+							<form method="post" action="includes/admin/act_borrower.php?id=<?php echo $_GET['id'] ?>">
+								<input type="submit" value="Borrow" class="btn btn-primary btn-medium" />
+							</form><?php //si on est connecté on peu emprunter
+						} else{
+							echo "<strong>Objet déjà emprunté</strong>";
+						}
+						
+						//include 'includes/object/borrow.php';//on affiche de quoi emprunter l'objet
+				 } else {
+						echo 'You should login to borrow this item'; // on doit se connecter
+					}
+				}    
+		?> 
+	</div>		
 </div> <!-- /container -->
     <?php include 'includes/footer.php'; ?>
