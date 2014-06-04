1a3ee33c9ff93167e7b0af7144d3eac49c024b2b
daliadadie <dalia33@hotmail.fr>
2014-06-04 13:32:46 +0200

    BBD

--- sql/user.sql
+++ sql/user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Mar 03 Juin 2014 à 22:52
--- Version du serveur :  5.6.16
--- Version de PHP :  5.5.11
+-- Client: localhost
+-- Généré le: Mer 04 Juin 2014 à 11:28
+-- Version du serveur: 5.6.12-log
+-- Version de PHP: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `projet`
+-- Base de données: `projet`
 --
 
 -- --------------------------------------------------------
@@ -28,28 +28,28 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `pseudo` varchar(255) NOT NULL,
-  `email` varchar(32) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `admin` tinyint(1) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `pseudo` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL,
   `phone` int(11) NOT NULL,
-  `adress` varchar(255) NOT NULL,
-  `photos` varchar(255) NOT NULL,
-  `network` varchar(255) NOT NULL,
-  `promo` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `adress` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `photos` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `network` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `promo` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pseudo` (`pseudo`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Contenu de la table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `lastname`, `pseudo`, `email`, `password`, `admin`, `phone`, `adress`, `photos`, `network`, `promo`) VALUES
-(12, 'azerrtt', 'julien', 'tumai', 'julien.bellec@hotmail.fr', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, 0, '', '', '', ''),
-(13, 'azer', 'ae', 'qsdfg', 'example@example.fr', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, 0, '', '', '', ''),
-(14, 'zerty', '', 'dalia', '', '6f7be09772f2e5d51923d5feaa84508ee8472f4e', NULL, 0, '', '', '', '');
+INSERT INTO `user` (`id`, `name`, `lastname`, `pseudo`, `email`, `password`, `phone`, `adress`, `photos`, `network`, `promo`) VALUES
+(12, 'bellec', 'julien', 'tumai', 'julien.bellec@hotmail.fr', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', 0, '', '', '', ''),
+(13, 'azer', 'ae', 'qsdfg', 'example@example.fr', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', 0, '', '', '', ''),
+(14, 'zerty', '', 'dalia', '', '6f7be09772f2e5d51923d5feaa84508ee8472f4e', 0, '', '', '', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
