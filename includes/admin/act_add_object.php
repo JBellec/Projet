580feb7d5be9a048b2a3b6e5d30d69f488987e3f
Tumai <julien.bellec@hotmail.fr>
2014-06-09 15:47:08 +0200

    changement add_object

---
+++ includes/admin/act_add_object.php
@@ -0,0 +1,59 @@
+<?php
+    /* Fichier appelé lors de l'inscription d'une personne */
+    
+    header('Content-type: text/html; charset=UTF-8');// Indique le bon format des entêtes (par défaut apache risque de les envoyer au standard ISO-8859-1)
+
+    $message = null;// Initialisation de la variable du message de réponse
+
+    // Récupération des variables issues du formulaire par la méthode post
+    $name = filter_input(INPUT_POST, 'name');
+	$type = filter_input(INPUT_POST, 'type');
+    $description = filter_input(INPUT_POST, 'description');
+    $cpu = filter_input(INPUT_POST, 'cpu');
+    $frequence = filter_input(INPUT_POST, 'frequence');
+    $ram = filter_input(INPUT_POST, 'ram');
+	$hard_drive = filter_input(INPUT_POST, 'hard_drive');
+	$gpu = filter_input(INPUT_POST, 'gpu');
+	$lenght = filter_input(INPUT_POST, 'lenght');
+	$number_port = filter_input(INPUT_POST, 'number_port');
+	echo 'ligne 18';
+	
+if (isset($name,$description)) {
+	include ('../mysql.php');
+	echo 'ligne 22';  
+                try
+                {
+                  
+                                // Pour enregistrer la date actuelle (date/heure/minutes/secondes) on peut utiliser directement la fonction mysql : NOW()
+                                $insertion = "INSERT INTO object(name,type, decription,  lenght, number_port, cpu, frequence, ram, hard_drive, gpu) VALUES(:name, :type, :description, :lenght, :number_port, :cpu, :frequence, :ram, :hard_drive, :gpu)";
+                               
+                                // préparation de l'insertion
+                                $insert_prep = $connect->prepare($insertion);
+                               
+                                // Exécution de la requête en passant les marqueurs et leur variables associées dans un tableau
+                                $inser_exec = $insert_prep->execute(array(':name'=>$name, ':type'=>$type,':description'=>$description, ':lenght'=>$lenght, ':number_port'=>$number_port, ':cpu'=>$cpu,':frequence'=>$frequence, ':ram'=>$ram, ':hard_drive'=>$hard_drive, ':gpu'=>$gpu));
+                               echo 'ligne 34';
+                                /* Si l'insertion s'est faite correctement...*/
+                                if ($inser_exec == true)
+                                {
+										echo'ligne38';
+                                       
+                                        // A MODIFIER Remplacer le '#' par l'adresse de votre page de destination, sinon ce lien indique la page actuelle.
+                                        $message = 'Object added.';
+                                        //ou redirection vers une page en cas de succès ex : menu.php
+                                        header("Location: ../object.php");
+                                        //exit();  
+                                 
+								}
+                }
+                catch (PDOException $e)
+                {
+                        $message = 'Problème dans la requête d\'insertion';
+                }      
+        }
+        else
+        {    // Au moins un des deux champs "pseudo" ou "mot de passe" n'a pas été rempli
+                $message = 'Les champs Name et Description doivent être remplis.';
+        }
+
+?>
--- includes/admin/add_object.php
+++ includes/admin/add_object.php
@@ -38,26 +38,55 @@
 			} else {/* On fait un accès direct depuis l'administration par exe */
 				if(isset($_SESSION['pseudo'])){
 							if($_GET['preset']=='PC'){
-						//$req2 = $connect->prepare('SELECT * from preset WHERE name= ?');
-						//$req2->execute(array($_GET['preset']));
 
-							while($result=$req->fetch()){
-		?>
-							<p><?php echo $result['name'];?></p>
-							<p><?php echo $result['description'];?></p>
-							<p><?php echo $result['cpu'];?></p>
-							<p><?php echo $result['frequence'];?></p>
-							<p><?php echo $result['ram'];?></p>
-							<p><?php echo $result['hard_drive'];?></p>
-							<p><?php echo $result['gpu'];?></p>
-							
-					<?php	
+					?>
+							<p class="lead">
+								<form method="post" action="includes/admin/act_add_object.php">
+									<div class="form-group">
+										<label>Name</label>
+										<input type="text" name ="name">
+									</div>
+									
+									<div class="form-group">
+										<label>Type</label>
+										<input type="text" name ="type">
+									</div>
+									<div class="form-group">
+										<label>Description</label>
+										<input type="text" name ="description">
+									</div>
+									<div class="form-group">
+										<label>CPU</label>
+										<input type="text" name="cpu">
+									</div>
+									<div class="form-group">
+										<label>Frequency</label>
+										<input type="text" name="frequence">
+									</div>
+									<div class="form-group">
+										<label>RAM</label>
+										<input type="text" name="ram">
+									</div>
+									<div class="form-group">
+										<label>Hard drive</label>
+										<input type="text" name="hard_drive">
+									</div>
+									<div class="form-group">
+										<label>GPU</label>
+										<input type="text" name="gpu">
+									</div>
+									<div class="form-group">
+										<input type="submit" value="Add">
+									</div>
+								</form>
+							</p>
+		<?php
 						}
 
 					}
 						
-						//include 'includes/object/borrow.php';//on affiche de quoi emprunter l'objet
-				 } else {
+
+				  else {
 						echo 'You should login to borrow this item'; // on doit se connecter
 					}
 				}    
