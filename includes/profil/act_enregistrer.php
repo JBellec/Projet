343280f3b400b72bbcc579975ff7a97fac6962f9
daliadadie <dalia33@hotmail.fr>
2014-06-10 18:04:26 +0200

    modif
    divers modif

--- includes/profil/act_enregistrer.php
+++ includes/profil/act_enregistrer.php
@@ -1,39 +1,56 @@
 <?php
 /*connexion a la bdd*/    
-  try {
-	    $connect = new PDO('mysql:host=localhost;dbname=projet', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-    }
-    catch (Exception $e) {
-	    die('Erreur : ' . $e->getMessage());
-    }
-if(isset($_POST['description']))
-{
-	/*ajout de ce qu'on a reçu*/
-	echo 'on ajoute';
-	$req= $connect->prepare('INSERT INTO user(description) VALUES(:description)');
-	$req->execute(array('description'=>$_POST['description']));
-	if($_FILES['photos']['error'] > 0)
-	{
-		echo 'erreur de transfert de la photo';
-	}
-	
-	/* traitement de l'image reçue */
-	
-		//1. strrchr renvoie l'extension avec le point (« . »).
-		//2. substr(chaine,1) ignore le premier caractère de chaine.
-		//3. strtolower met l'extension en minuscules.
-	$extension_upload = strtolower(  substr(  strrchr($_FILES['photos']['name'], '.')  ,1)  );
-	$nom = "image/nomphoto.{$extension_upload}";
-	$resultat = move_uploaded_file($_FILES['photos']['tmp_name'],$nom);
-	if ($resultat)
-	{
-		header('location:../../index.php');
-	}
-	else
-	{
-		echo 'echec du transfert de l\'image';
-	}	
-}
+ include '../mysql.php';
+     $description = filter_input(INPUT_POST, 'description');
+     $titre = filter_input(INPUT_POST, 'titre');
+     $pseudoactuel = filter_input(INPUT_POST, 'pseudoactuel');
+	 if(isset($_POST['titre'])&&isset($_POST['description'])&&isset($_POST['pseudoactuel']))
+	 {
+				 echo 'ligne 14';
+					/*ajout de ce qu'on a reçu*/
+					echo 'on ajoute';
+					$req= $connect->prepare('UPDATE user SET titre = :nvtitre, description=:nvdescription WHERE pseudo = :pseudoactuel');
+					$req->execute(array(
+						   'nvtitre'=>$titre,
+						   'nvdescription'=>$description,
+						   'pseudoactuel'=>$pseudoactuel
+						   ));
+					
+					if($_FILES['image']['error'] > 0)
+					{
+						echo 'erreur de transfert de l\'image';
+					}
+					
+					/* traitement de l'image reçue */
+					
+						//1. strrchr renvoie l'extension avec le point (« . »).
+						//2. substr(chaine,1) ignore le premier caractère de chaine.
+						//3. strtolower met l'extension en minuscules.
+					$extension_upload = strtolower(  substr(  strrchr($_FILES['image']['name'], '.')  ,1)  );
+					$nom_img=$_POST['titre'];
+					$nom = "img/{$nom_img}.{$extension_upload}";
+					$resultat = move_uploaded_file($_FILES['image']['tmp_name'],$nom);
+					$image= $nom;
+					
+					if ($resultat)
+					{
+						echo 'transfert réussi !';
+						
+				       $req = $connect->prepare('UPDATE user set image = :nvimage  WHERE pseudo =:pseudoactuel');
+				       $req->execute(array(
+					      'nvimage' => $image,
+					      'pseudoactuel'=>$pseudoactuel,
+					      ));
+				        header('location:../../profil.php');
+				        echo 'La photo a bien été ajouté !';
+
+
+					}
+					else
+					{
+						echo 'echec du transfert de l\'image';
+					}	
+      }
 else
 {
 	echo 'il faut remplir tous les champs ! =)';
--- includes/profil/parcourir.php
+++ includes/profil/parcourir.php
@@ -1,18 +1,18 @@
 
-		<title>QR-Ensisa</title>
-	</head>  
+		<title>QR-Ensisa</title>  
 <body>
-	
-	<form method="post" action="act_enregistrer.php" enctype="multipart/form-data">
-	
-     <label for="icone">Image (JPG, PNG ou GIF | max. 15 Ko) :</label><br />
-     <input type="file" name="icone" id="icone" /><br />
-     
-     <label for="description">Description de votre fichier (max. 255 caractères) :</label><br />
-     <textarea name="description" id="description"></textarea><br />
-     <input type="submit" name="submit" value="Envoyer" />
-</form>
-
- 
-
+	<form>
+	        <fieldset>
+					<legend><h2>ajout d'un projet : </h2></legend>
+					<label><strong>titre</strong></label></br>
+					<input type="text" size="16" name="titre"/></br>
+					<label><strong>image du projet (max. 1 Mo format jpg) : </strong></label></br>
+					<input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+					<input type="file" name="image"/></br>
+					<label><strong>description du projet : </strong></label></br>
+					<textarea name="description" rows="10" cols="70"></textarea></br>
+					<input type="submit" value="ajouter"/></br></br>
+			</fieldset>
+    
+    </form>
 </body>
