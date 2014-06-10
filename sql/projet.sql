bd12008b27da98584c837eaf048ce0065e01a359
zoski <zoski@free.fr>
2014-06-10 16:47:21 +0200

    Correctionqrcode

--- sql/projet.sql
+++ sql/projet.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Ven 30 Mai 2014 à 16:32
+-- Généré le :  Mar 10 Juin 2014 à 15:44
 -- Version du serveur :  5.6.16
 -- Version de PHP :  5.5.11
 
@@ -23,17 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `admin`
---
-
-CREATE TABLE IF NOT EXISTS `admin` (
-  `pseudo` varchar(255) NOT NULL,
-  UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tables des utilisateur qui sont aussi administrateur';
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `borrowed`
 --
 
@@ -42,7 +31,15 @@ CREATE TABLE IF NOT EXISTS `borrowed` (
   `pseudo` varchar(255) NOT NULL,
   `object_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table qui fait la correspondance entre les objets emrpunté et l''utilisateur' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table qui fait la correspondance entre les objets emrpunté et l''utilisateur' AUTO_INCREMENT=16 ;
+
+--
+-- Contenu de la table `borrowed`
+--
+
+INSERT INTO `borrowed` (`id`, `pseudo`, `object_id`) VALUES
+(14, 'zoski', 8),
+(15, 'zoski', 7);
 
 -- --------------------------------------------------------
 
@@ -60,18 +57,30 @@ CREATE TABLE IF NOT EXISTS `object` (
   `number_port` int(11) NOT NULL,
   `cpu` varchar(512) NOT NULL,
   `frequence` float NOT NULL,
-  `ram` int(11) NOT NULL,
+  `ram` float NOT NULL,
   `hard_drive` int(11) NOT NULL,
   `gpu` varchar(512) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table qui stock les information sur les objets du Xid' AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table qui stock les information sur les objets du Xid' AUTO_INCREMENT=14 ;
 
 --
 -- Contenu de la table `object`
 --
 
 INSERT INTO `object` (`id`, `name`, `description`, `type`, `borrowed`, `lenght`, `number_port`, `cpu`, `frequence`, `ram`, `hard_drive`, `gpu`) VALUES
-(1, 'Kirby', 'L''ordinateur Kirby', 'PC', 0, NULL, 0, '4 Core AMD', 3.4, 4, 500, 'Radeon R9 260');
+(1, 'Kirby', 'L''ordinateur Kirby', 'PC', 0, NULL, 0, '4 Core AMD', 3.4, 4, 500, 'Radeon R9 260'),
+(2, 'Samus', 'L''ordinateur Samus', 'PC', 0, NULL, 0, '4 Core AMD', 3.4, 4, 500, 'Radeon R9 260'),
+(3, 'Lorien', 'Raspberry Pi', 'PC', 0, NULL, 0, 'AMR 7', 0.8, 0.5, 16, 'Intégré'),
+(4, 'XidDemo', 'Raspberry Pi 2', 'PC', 0, NULL, 0, 'AMR 7', 0.8, 0.5, 4, 'Intégré'),
+(5, 'Pac Man', 'L''ordinateur Pac Man', 'PC', 0, NULL, 0, 'Intel P4 Double core', 2.5, 2, 500, 'Nvidia Geforce 9600 GT'),
+(6, 'Pong', 'L''ordinateur Pong, Xubuntu, StepMania', 'PC', 0, NULL, 0, 'Intel P4 Dual Core', 2.4, 1, 80, 'A définir'),
+(7, 'Sonic', 'L''ordinateur Sonic - Windows 7', 'PC', 1, NULL, 0, 'Intel P4 Double core', 2.4, 2, 500, 'Radeon'),
+(8, 'Tetris', 'L''ordinateur Tetris - Windows 7', 'PC', 1, NULL, 0, 'Intel P4 Double core', 2.4, 2, 500, 'Radeon'),
+(9, 'Zelda', 'L''ordinateur Zelda - Windows 7', 'PC', 0, NULL, 0, 'Intel P4 Double core', 2.4, 2, 500, 'Radeon'),
+(10, 'Fangorn', 'Serveur FTP', 'PC', 0, NULL, 0, 'Mono Core', 3.4, 1, 170, 'Sans'),
+(11, 'Fomcombe', 'Serveur DHCP etc...', 'PC', 0, NULL, 0, 'Mono Core', 3.4, 1, 170, 'Sans'),
+(12, 'Erebor', 'Serveur Proxmox', 'PC', 0, NULL, 0, 'Mono Core', 3.4, 8, 170, 'Sans'),
+(13, 'Gondor', 'Serveur Proxmox', 'PC', 0, NULL, 0, 'Intel Xeon', 3, 8, 74, 'Sans');
 
 -- --------------------------------------------------------
 
@@ -91,14 +100,16 @@ CREATE TABLE IF NOT EXISTS `preset` (
   `hard_drive` tinyint(1) NOT NULL,
   `gpu` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table qui stock les preset pour chaque type d''objects' AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table qui stock les preset pour chaque type d''objects' AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `preset`
 --
 
 INSERT INTO `preset` (`id`, `name`, `description`, `lenght`, `number_port`, `cpu`, `frequence`, `ram`, `hard_drive`, `gpu`) VALUES
-(1, 'PC', 'Les ordinateurs du Xid', 0, 0, 1, 1, 1, 1, 1);
+(1, 'PC', 'Les ordinateurs du Xid', 0, 0, 1, 1, 1, 1, 1),
+(2, 'Switch', 'Les Switch du Xid', 0, 1, 1, 1, 1, 0, 0),
+(4, 'Multiprise', 'Les mutiprises du xid, grande ou pas...', 1, 1, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -108,22 +119,28 @@ INSERT INTO `preset` (`id`, `name`, `description`, `lenght`, `number_port`, `cpu
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `prenom` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8 NOT NULL,
   `pseudo` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 NOT NULL,
   `password` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `admin` int(4) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+  `phone` int(11) NOT NULL,
+  `adress` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `photos` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `network` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `promo` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pseudo` (`pseudo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Contenu de la table `user`
 --
 
-INSERT INTO `user` (`id`, `nom`, `prenom`, `pseudo`, `email`, `password`, `admin`) VALUES
-(12, 'Bellec', 'Julien', 'Tumai', 'julien.bellec@hotmail.fr', 'soncssimmam', 0),
-(13, 'Alberola', 'Gaël', 'zoski', 'zoski@free.fr', '64faf5d0b1dc311fd0f94af64f6c296a03045571', 1);
+INSERT INTO `user` (`id`, `name`, `lastname`, `pseudo`, `admin`, `email`, `password`, `phone`, `adress`, `photos`, `network`, `promo`) VALUES
+(12, 'bellec', 'julien', 'tumai', 0, 'julien.bellec@hotmail.fr', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', 0, '', '', '', ''),
+(15, 'Alberola', 'Gaël', 'zoski', 1, 'zoski@free.fr', '64faf5d0b1dc311fd0f94af64f6c296a03045571', 0, '', '', '', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
