6711691e72eec0ebb7a1f887f55949366388b6f4
Tumai <julien.bellec@hotmail.fr>
2014-06-10 17:21:20 +0200

    add_object again

--- includes/admin/add_object.php
+++ includes/admin/add_object.php
@@ -1,14 +1,13 @@
 <h2 class="sub-header">Add an object</h2>
-<div class="container">
-<li><a href="admin.php?sec=add_object&preset=PC">Add a PC</a></li>
-<li><a href="admin.php?sec=add_object&preset=cable_ethernet">Add a cable</a></li>
-<li><a href="admin.php?sec=add_object&preset=switch">Add a switch</a></li>
-
-
 <?php
-	if(isset($_GET['preset'])){
-		include 'add_object_cible.php';
-	}
+$req -> query('select name from preset');
+<div class="container">
+
+while($fectched = $req->fetch()){
+echo '<li><a href="admin.php?sec=add_object&preset=PC">Add a $fetched['name']</a></li>'
+}
 ?>
+
+
 </div> <!-- /container -->
 
