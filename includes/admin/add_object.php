0a8569349fe046dd727e37eb7decb2caf995bb59
Tumai <julien.bellec@hotmail.fr>
2014-06-10 17:41:06 +0200

    add_object ok ça roule niquel tout bien

--- includes/admin/add_object.php
+++ includes/admin/add_object.php
@@ -1,10 +1,11 @@
 <h2 class="sub-header">Add an object</h2>
 <?php
-$req -> query('select name from preset');
+$req= $connect -> query('select name from preset');
+?>
 <div class="container">
-
-while($fectched = $req->fetch()){
-echo '<li><a href="admin.php?sec=add_object&preset=PC">Add a $fetched['name']</a></li>'
+<?php
+while($fetched = $req->fetch()){
+echo '<li><a href="admin.php?sec=add_object&preset=PC">Add a '.$fetched['name'];
 }
 ?>
 
