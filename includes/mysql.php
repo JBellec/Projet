d7b0696094d4592c5563ed696c0d666d55362790
daliadadie <dalia33@hotmail.fr>
2014-06-10 18:14:25 +0200

    de la merde

--- includes/mysql.php
+++ includes/mysql.php
@@ -1,6 +1,6 @@
 <?php
     try {
-	    $connect = new PDO('mysql:host=localhost;dbname=qr', 'qr', 't67TvNSzC5XGcV7a', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+	    $connect = new PDO('mysql:host=localhost;dbname=projet', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
     }
     catch (Exception $e) {
 	    die('Erreur : ' . $e->getMessage());
